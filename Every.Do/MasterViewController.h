83142732b695403d477839a01e70ec7adcf5de64
Shahin Zangenehpour <98chimp@users.noreply.github.com>
2015-03-25 12:46:19 -0700

    Initial Commit

---
+++ Every.Do/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Every.Do
+//
+//  Created by Shahin on 2015-03-25.
+//  Copyright (c) 2015 98% Chimp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
